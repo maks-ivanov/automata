c3835a5dbc319ec8ff4ffc525f3b522c87d2bc91
Owen Colegrove <owen@algofi.org>
2023-05-29 18:20:07 -0400

    Add indexing tests

---
+++ automata/core/code_indexing/test/sample_modules/local_module_2.py
@@ -0,0 +1,20 @@
+"""NriSjAbKkquTaPtwOXEE"""
+import random
+
+
+class JSztF:
+    """ROJHXYiUEZjBjVWQKkOb"""
+
+    def __init__(self):
+        pass
+
+    def method(self):
+        """iSGQiFFhiIlVnOuMkLxa"""
+
+        pass
+
+
+def TtAsi():
+    """dnjZpciGdTqBMzmtWHVU"""
+
+    pass
---
+++ automata/core/code_indexing/test/sample_modules/local_module_3.py
@@ -0,0 +1,5 @@
+def multi_line_doc_function() -> bool:
+    """This is a function with a multi-line docstring.
+
+    It should be handled correctly by the get_docstring method."""
+    return True
